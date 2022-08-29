9e6e6bdb15bde5f6a10a7788f6768dd070d967cf
Day Vazquez <dayvazquez@protonmail.com>
2022-08-29 18:29:19 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,4 @@
-# Fibonacci series: the sum of two elements define the next
+# The Fibonacci sequence is obtained by adding the previous two consecutive terms
 #
 # Function that takes a natural number as its input, and
 # returns the value of that fibonacci number.
@@ -19,11 +19,10 @@ def faster_fibonacci(n):
     return current
 
 def fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 def main():
     t = TestTools()
