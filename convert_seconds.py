6b210a7274b950eedce4e247e4cc5847588d0d1f
Day Vazquez <dayvazquez@protonmail.com>
2022-08-30 11:15:25 +0200

    Update convert_seconds.py

--- convert_seconds.py
+++ convert_seconds.py
@@ -1,15 +1,9 @@
-# Write a procedure, convert_seconds, which takes as input a non-negative 
+# Function that takes as input a non-negative 
 # number of seconds and returns a string of the form 
 # '<integer> hours, <integer> minutes, <number> seconds' but
 # where if <integer> is 1 for the number of hours or minutes, 
 # then it should be hour/minute. Further, <number> may be an integer
 # or decimal, and if it is 1, then it should be followed by second.
-# You might need to use int() to turn a decimal into a float depending
-# on how you code this. int(3.0) gives 3
-#
-# Note that English uses the plural when talking about 0 items, so
-# it should be "0 minutes".
-#
 
 def convert_seconds(n):
     hours = n // 3600
