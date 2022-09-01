0511acf14c604d9b47d0fe8cf354f55d1e78bdf1
Day Vazquez <dayvazquez@protonmail.com>
2022-09-01 20:12:20 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,28 @@
 # The Fibonacci sequence is obtained by adding the previous two consecutive terms
 #
-# Function that takes a natural number as its input, and
-# returns the value of that fibonacci number.
-#
 # Two Base Cases: fibonacci(0) => 0 ; fibonacci(1) => 1
 # Recursive Case:
 #                n > 1 : fibonacci(n) => fibonacci(n-1) + fibonacci(n-2)
 #
-###############################################################################
+###############################################################################################################
+#
+# The first and second terms of the Fibonacci sequence are 0 and 1 respectively. 
+# Each successive term in the sequence is then calculated by adding the two preceding terms. 
+# That is, the nth term is calculated by adding the (n-2)th and (n-1)th terms. 
+# Thus, store the already computed sequence in a list to be able to calculate each new element in the sequence.
+def fibonacci(n):
+  myArray = []
+  for i in range(n):
+    if i is 0 or i is 1:
+      myArray.append(i)
+      yield i
+    else:
+      x = myArray[i-2] + myArray[i-1]
+      myArray.append(x)
+      yield x
+      
+for i in fibonacci(8):
+  print(i)
 
 from UnitaryTest.test_tools import TestTools
 
@@ -18,6 +33,7 @@ def faster_fibonacci(n):
         current, after = after, current + after # a, b = b, a+b
     return current
 
+# Function that takes a natural number as its input, and returns the value of that fibonacci number.
 def fibonacci(n):
     if n <= 1:
         return n
