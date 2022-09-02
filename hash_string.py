96e47fbf462fdcbcdb197bb3c164eff70ab402ec
Day Vazquez <dayvazquez@protonmail.com>
2022-09-02 23:33:14 +0200

    Minor changes

--- hash_string.py
+++ hash_string.py
@@ -1,17 +1,11 @@
-# Define a function, hash_string,
-# that takes as inputs a keyword
-# (string) and a number of buckets,
-# and returns a number representing
-# the bucket for that keyword.
+# Function that takes as inputs a keyword (string) and a number of buckets,
+# and returns a number representing the bucket for that keyword.
 
 def hash_string(keyword,buckets):
     output = 0 
     for i in keyword:
         output += ord(i)
     return output % buckets
-
-
-
 
 print(hash_string('a',12))
 #>>> 1
