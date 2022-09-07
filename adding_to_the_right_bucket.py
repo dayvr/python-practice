0d25a587ba2528f209368c86c0c80df625e012d6
Day Vazquez <dayvazquez@protonmail.com>
2022-09-07 10:38:04 +0200

    Edit comments

--- adding_to_the_right_bucket.py
+++ adding_to_the_right_bucket.py
@@ -1,28 +1,8 @@
-#Define a procedure, hashtable_add(htable,key,value)
-#that adds the key to the hashtable (in 
-# the correct bucket), with the correct 
-# value and returns the new hashtable.
-#(Note that the video question and answer
-#  do not return the hashtable, but your code
-#  should do this to pass the test cases.)
-#-------------------------------------------
-# Define a procedure,
-# hashtable_lookup(htable,key)
-# that takes two inputs, a hashtable
-# and a key (string),
-# and returns the value associated
-# with that key.
-#-------------------------------------------
-# Define a procedure,
-# hashtable_update(htable,key,value)
-# that updates the value associated with key. If key is already in the
-# table, change the value to the new value. Otherwise, add a new entry
-# for the key and value.
-# Hint: Use hashtable_lookup as a starting point.
-# Make sure that you return the new htable
-
 from UnitaryTest.test import evaluate_result
 
+# Updates the value associated with key. If key is already in the
+# table, change the value to the new value. Otherwise, add a new entry
+# for the key and value.
 def hashtable_update(htable,key,value):
     existent = hashtable_lookup(htable, key)
     bucket = hashtable_get_bucket(htable, key)
@@ -45,6 +25,8 @@ def hashtable_update_2(htable,key,value):
         hashtable_add(htable, key, value)
     return htable
 
+# Takes two inputs, a hashtable and a key (string),
+# and returns the value associated with that key.
 def hashtable_lookup_list(htable,key):
     value = None
     bucket = hashtable_get_bucket(htable, key)
@@ -63,6 +45,8 @@ def hashtable_lookup(htable,key):
             break
     return value
 
+# Adds the key to the hashtable (in the correct bucket), with the correct 
+# value and returns the new hashtable.
 def hashtable_add(htable,key,value):
     bucket = hashtable_get_bucket(htable, key)
     bucket.append([key, value])
