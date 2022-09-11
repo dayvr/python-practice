f48f36495328695207b8c9828edc46d48fa41531
Day Vazquez <dayvazquez@protonmail.com>
2022-09-11 21:46:21 +0200

    Refactoring file

--- deep_count.py
+++ deep_count.py
@@ -1,25 +1,18 @@
 # Deep Count 
 
-# The built-in len operator outputs the number of top-level elements in a List,
-# but not the total number of elements. For this question, your goal is to count
-# the total number of elements in a list, including all of the inner lists.
-
-# Define a procedure, deep_count, that takes as input a list, and outputs the
-# total number of elements in the list, including all elements in lists that it
-# contains.
-
-
-# For this procedure, you will need a way to test if a value is a list. We have
-# provided a procedure, is_list(p) that does this:
+# The built-in len() operator outputs the number of top-level elements in a List,
+# but not the total number of elements. 
+# The goal here is to count the total number of elements in a list, including all of the inner lists.
 
 from UnitaryTest.test_tools import TestTools
 
+# Test if a value is a list. 
+# It returns True if the input is a List, and returns False otherwise.
 def is_list(p):
     return isinstance(p, list)
 
-# It is not necessary to understand how is_list works. It returns True if the
-# input is a List, and returns False otherwise.
-
+# Takes as input a list, and outputs the total number of elements in the list, 
+# including all elements in lists that it contains.
 def deep_count(p):
     count = 0
     for i in p:
