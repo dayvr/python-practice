45091320e9ab60d7ffeb6ac80ccf08d41febdfc4
Day Vazquez <dayvazquez@protonmail.com>
2022-09-12 14:00:18 +0200

    Comment changes

--- pick_one.py
+++ pick_one.py
@@ -1,19 +1,16 @@
-# Question 1: Pick One
-#
-# Define a procedure, pick_one, that takes three inputs: a Boolean 
-# and two other values. If the first input is True, it should return 
-# the second input. If the first input is False, it should return the 
-# third input.
-#
-# For example, pick_one(True, 37, 'hello') should return 37, and
-# pick_one(False, 37, 'hello') should return 'hello'.
 from UnitaryTest.test_tools import TestTools
+
+# Takes three inputs: a Boolean and two other values. 
+# If the first input is True, it should return the second input. 
+# If the first input is False, it should return the third input.
+#
+# For example, pick_one(True, 37, 'hello') will return 37, and
+# pick_one(False, 37, 'hello') will return 'hello'.
 
 def pick_one(boolean, second_value, third_value):
     if boolean:
         return second_value
     return third_value
-
 
 def main():
     t = TestTools()
