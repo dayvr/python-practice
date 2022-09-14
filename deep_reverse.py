b2338908b559578c89a2bcc6fffd8246d1ce7c70
Day Vazquez <dayvazquez@protonmail.com>
2022-09-14 14:16:06 +0200

    Refactor some comments

--- deep_reverse.py
+++ deep_reverse.py
@@ -1,19 +1,14 @@
-# Deep Reverse
-# Define a procedure, deep_reverse, that takes as input a list, 
-# and returns a new list that is the deep reverse of the input list.  
-# This means it reverses all the elements in the list, and if any 
-# of those elements are lists themselves, reverses all the elements 
-# in the inner list, all the way down. 
-
-# Note: The procedure must not change the input list.
-
-# The procedure is_list below is from Homework 6. It returns True if 
-# p is a list and False if it is not.
 from UnitaryTest.test_tools import TestTools
 
+# Deep Reverse
+
+# Return True if p is a list and False if it is not.
 def is_list(p):
     return isinstance(p, list)
 
+# As input a list, returns a new list that is the deep reverse of the input list. 
+# This means it reverses all the elements in the list, and if any of those elements 
+# are lists themselves, reverses all the elements in the inner list, all the way down. 
 def deep_reverse(l):
     rev = []
     for i in range(len(l)-1, -1, -1):
