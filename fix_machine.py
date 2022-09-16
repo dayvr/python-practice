78f000ebc1855a8dec043a262fb5c9827ee3582c
Day Vazquez <dayvazquez@protonmail.com>
2022-09-16 15:47:57 +0200

    Update comments

--- fix_machine.py
+++ fix_machine.py
@@ -1,28 +1,10 @@
-# That freaking superhero has been frequenting Udacity
-# as his favorite boss battle fight stage. The 'Udacity'
-# banner keeps breaking, and money is being wasted on
-# repairs. This time, we need you to proceduralize the
-# fixing process by building a machine to automatically
-# search through debris and return the 'Udacity' banner
-# to the company, and be able to similarly fix other goods.
-
-# Write a Python procedure fix_machine to take 2 string inputs
-# and returns the 2nd input string as the output if all of its
+from UnitaryTest.test_tools import TestTools
+# Exercise from Udacity course.
+# Takes 2 string inputs and returns the 2nd input string as the output if all of its
 # characters can be found in the 1st input string and "Give me
 # something that's not useless next time." if it's impossible.
 # Letters that are present in the 1st input string may be used
-# as many times as necessary to create the 2nd string (you
-# don't need to keep track of repeat usage).
-# TOOLS: # if statement
-         # while loop
-         # string operations
-         # Unit 1 Basics
-
-# BONUS: # 
-# 5***** #  If you've graduated from CS101,
-#  Gold  #  try solving this in one line.
-# Stars! #
-from UnitaryTest.test_tools import TestTools
+# as many times as necessary to create the 2nd string.
 
 def fix_machine(debris, product):
     i = 0
